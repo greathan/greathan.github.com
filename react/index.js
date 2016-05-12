1d97819f690f558f082180e0668860fbed31c651
Great Han <hwlyglove@gmail.com>
2016-05-12 15:48:17 +0800

    submit

---
+++ react/index.js
@@ -0,0 +1,56 @@
+
+
+
+var Flight = React.createClass({
+	render: function() {
+		return (
+			<div className="flight">
+				{this.props.co}
+			</div>
+		);
+	}
+});
+
+
+
+var FlightList = React.createClass({
+	render: function() {
+
+		var flights = [];
+
+		for (var key in this.props.flightInfo) {
+
+			var flt = this.props.flightInfo[key];
+			flights.push(
+				<Flight co={flt.co}>
+				</Flight
+			)
+
+		}
+
+		return (
+
+			<div className="flightList">
+				{flights}
+			</div>
+
+		)
+
+	}
+});
+
+var Box = React.createClass({
+  render: function() {
+    return (
+      <div className="box">
+        <h1>Flights</h1>
+        <FlightList data={this.props.data} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Box url="data.json" />,
+  document.getElementById('content')
+);
