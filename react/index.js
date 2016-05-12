b396736ac4672c618a7d2f0bad6a7053048256b4
Great Han <hwlyglove@gmail.com>
2016-05-12 17:19:46 +0800

    submit

--- react/index.js
+++ react/index.js
@@ -5,7 +5,10 @@ var Flight = React.createClass({
 	render: function() {
 		return (
 			<div className="flight">
-				{this.props.co}
+				<div className="ca"><img src={this.props.ca_url} /></div>
+				<div className="co">{this.props.co}</div>
+				<div className="time">{this.props.dt} - {this.props.at}</div>
+				<div className="price">&yen;{this.props.price}</div>
 			</div>
 		);
 	}
@@ -16,13 +19,16 @@ var Flight = React.createClass({
 var FlightList = React.createClass({
 	render: function() {
 
-		var flights = [];
+		var flights = [],
+			flightInfo = this.props.data.flightInfo,
+			priceInfo = this.props.data.priceInfo;
 
-		for (var key in this.props.flightInfo) {
+		for (var fn in flightInfo) {
 
-			var flt = this.props.flightInfo[key];
-			var flight = <Flight co={flt.co} />
-			flights.push(flight);
+			var flt = flightInfo[fn],
+				price = priceInfo[fn];
+			var ca_url = 'http://simg1.qunarzz.com/site/images/airlines/' +flt.ca+ '.gif';
+			flights.push(<Flight co={flt.co} price={price.lownpr} dt={flt.dt} at={flt.at} ca_url={ca_url} />);
 
 		}
 
@@ -37,18 +43,34 @@ var FlightList = React.createClass({
 	}
 });
 
-var Box = React.createClass({
-  render: function() {
-    return (
-      <div className="box">
-        <h1>Flights</h1>
-        <FlightList data={this.props.data} />
-      </div>
-    );
-  }
+var FlightBox = React.createClass({
+
+	getInitialState: function() {
+		return {data: {}};
+	},
+	componentDidMount: function() {
+
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			success: function(data) {
+				this.setState({data: data.oneway_data});
+			}.bind(this)
+		})
+
+	},
+	render: function() {
+		return (
+		  <div className="box">
+		    <h1>Flights</h1>
+		    <FlightList data={this.state.data} />
+
+		  </div>
+		);
+	}
 });
 
 ReactDOM.render(
-  <Box url="data.json" />,
+  <FlightBox url="data.json" />,
   document.getElementById('content')
 );
